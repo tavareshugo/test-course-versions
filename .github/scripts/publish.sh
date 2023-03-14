32851a21a60da87428b8d7d15c31734d253d6d08
Hugo Tavares <hm533@cam.ac.uk>
2023-03-14 16:24:35 +0000

    new build workflow

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# create appendix.md file
+echo "
+---
+title: "Archive"
+---
+
+:::{.callout-tip}
+#### Archived Versions
+
+This page lists links to archived versions of these materials, which ran as a live course on the given date. 
+We highly recommend that you refer to the latest version of the materials, however you can use these as a reference if you attended one of our past live courses. 
+:::
+
+" > archive.md
+
+# download latest 4 releases
+while read tag
+do 
+  # get remote URL
+  url=$(git config --get remote.origin.url | sed 's/.*://' | sed 's/.git$//')
+  url=$(echo "https://github.com/$url/archive/refs/tags/${tag}.zip")
+  
+  # create directory
+  mkdir -p archive/${tag}
+  
+  # download, unzip and move to directory
+  wget -O ${tag}.zip $url
+  unzip -d archive/${tag}/ ${tag}.zip
+  
+  # add to archive.md file
+  date=$(echo $tag | sed 's/v//' | sed 's/\./-/g')
+  echo "* [${date}](archive/${tag}/index.html)" >> archive.md
+  
+done < <(grep -v "#" .github/releases.txt | head -n 5 | tail -n 4)
+
+# add appendix to _quarto.yml
 echo "
   appendices:
     - text: Archive
--- archive.md
+++
@@ -1,6 +0,0 @@
----
-title: "Archive"
----
-
-* [2022-01-01](archive/2022-01-01/index.html)
-* [2023-01-01](archive/2023-01-01/index.html)
